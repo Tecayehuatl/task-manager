112950ee57cdd6bfa52956e2bd0d998e6d153aa1
Enrique Delgado Tecayehuatl <enrique.delgado@live.com>
2024-03-28 17:34:20 -0600

    Base route array added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { provideState } from '@ngrx/store';
+import { dashboardKey, dashboardReducer } from './store/reducers/dashboard.reducer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    providers: [
+      provideState({ name: dashboardKey, reducer: dashboardReducer })
+    ]
+  }
+];
