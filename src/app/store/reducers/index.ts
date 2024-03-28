563a8d5e807c3e5c44d61302dc17b5a64d313bd8
Enrique Delgado Tecayehuatl <enrique.delgado@live.com>
2024-03-28 17:32:58 -0600

    Unused file removed

--- src/app/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
-import { isDevMode } from '@angular/core';
-import { AppState, appReducers } from './app.reducers';
-
-export const reducers: ActionReducerMap<AppState> = {
-  appState: appReducers
-};
-export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [] : [];
