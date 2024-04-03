6df6de7f357f46be63e4a5cfb5d7fd4c137985d6
Enrique Delgado Tecayehuatl <enrique.delgado@live.com>
2024-04-02 20:19:43 -0600

    refactor(View Task): Task passed to the form in order to be created the current task

--- src/app/shared/view-task/view-task.component.ts
+++ src/app/shared/view-task/view-task.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { TaskStatus } from '../../dashboard/dashboard.component';
+import { Task, TaskStatus } from '../../dashboard/dashboard.component';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
-import { SubtaskComponent } from '../components/subtask/subtask.component';
-import { StrikeThroughDirective } from '../directives/strike-through.directive';
 
 @Component({
   selector: 'app-view-task',
@@ -19,12 +17,8 @@ import { StrikeThroughDirective } from '../directives/strike-through.directive';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatSelectModule,
-    // MatInputModule,
-    // MatIconModule,
     MatButtonModule,
     MatCheckboxModule,
-    SubtaskComponent,
-    StrikeThroughDirective,
   ],
   templateUrl: './view-task.component.html',
   styleUrl: './view-task.component.scss'
@@ -32,35 +26,68 @@ import { StrikeThroughDirective } from '../directives/strike-through.directive';
 export class ViewTaskComponent implements OnInit {
   taskForm!: FormGroup;
   taskStatus = TaskStatus;
+  totalSubTaskDone = 0;
 
   get subtasksControl(): FormArray {
     return this.taskForm.get('subtasks') as FormArray;
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<ViewTaskComponent>,
+    @Inject(MAT_DIALOG_DATA) public task: Task,
+  ) {}
 
   ngOnInit(): void {
     this.createAddTaskForm();
+    this.totalSubTaskDone = this.getTotalSubTaskDone(this.subtasksControl.value);
+    this.listenSubtasksChanges();
   }
 
   createAddTaskForm(): void {
     this.taskForm = this.fb.group({
       subtasks: this.fb.array([
-        {
-          title: 'test 1',
-          isDone: false,
-        },
-        {
-          title: 'test 2',
-          isDone: false,
-        },
 
       ]),
       status: [null, Validators.required],
     });
+
+    // Creating the subtask items
+    this.task.subtasks?.map(({ title, status }) => {
+        this.subtasksControl.push(this.creaetSubTaskGroup(title, status));
+    });
+
+    this.taskForm.get('status')?.patchValue(this.task.status);
   }
 
-  getSubtaskValueForm(index: number) {
-    return this.subtasksControl.controls[index];
+  getSubTaskControlValue(index: number, property: string): boolean {
+    return this.subtasksControl.controls[index].value?.[property];
+  }
+
+  getTotalSubTaskDone(subtasks: []): number {
+    if (subtasks.length === 0)
+      return 0;
+
+    let totalDone = 0;
+
+    subtasks.map(({ isDone }) => {
+      if (isDone) {
+        totalDone += 1;
+      }
+    });
+    return totalDone;
+  }
+
+  listenSubtasksChanges(): void {
+    this.subtasksControl.valueChanges.subscribe((value)=> {
+      this.totalSubTaskDone = this.getTotalSubTaskDone(value);
+    });
+  }
+
+  creaetSubTaskGroup(title: string, status: TaskStatus): FormGroup {
+    return  this.fb.group({
+      title: [title],
+      isDone: [status === 'done'? true: false],
+    })
   }
 }
