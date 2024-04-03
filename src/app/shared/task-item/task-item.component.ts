c8bdbc1d4efe841c4c880437299b9fc15530e73b
Enrique Delgado Tecayehuatl <enrique.delgado@live.com>
2024-04-02 20:17:31 -0600

    feature(Task Item): Show tasks marked as done and the length of the current subtasks of the main task

--- src/app/shared/task-item/task-item.component.ts
+++ src/app/shared/task-item/task-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Task } from '../../dashboard/dashboard.component';
+import { Component, Input, OnInit } from '@angular/core';
+import { Task, TaskStatus } from '../../dashboard/dashboard.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 
@@ -10,6 +10,25 @@ import { MatDividerModule } from '@angular/material/divider';
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss'
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
+  totalSubTaskDone: number = 0;
+
+  ngOnInit(): void {
+    this.totalSubTaskDone = this.getTotalSubTaskDone(this.task?.subtasks || []);
+  }
+
+  getTotalSubTaskDone(subtasks: Task[]): number {
+    if (subtasks.length === 0)
+      return 0;
+
+    let totalDone = 0;
+
+    subtasks.map(({ status }) => {
+      if (status === TaskStatus.DONE) {
+        totalDone += 1;
+      }
+    });
+    return totalDone;
+  }
 }
